4849cb96e1115f9e1ad55d7f2f3167235671a830
Luke Stanislaus <luke@stanislaus.co.uk>
2019-04-14 20:41:50 +0100

    begin new database

--- backend/src/graphql.js
+++ backend/src/graphql.js
@@ -8,17 +8,56 @@ const typeDefs = gql`
   type RootQuery {
     allBoatData: [BoatData]
   }
-  schema{
+  schema {
     query: RootQuery
     mutation: RootMutation
   }
   type RootMutation {
     updateBoatData(input: UpdateBoatDataInput!): UpdateBoatDataPayload
+    createRaces(input: CreateRacesInput!): CreateRacesPayload
   }
   type BoatData {
       boatName: String!
       crew: Int!
       pY: Int!
+  }
+  type CreateRacesPayload{
+
+  }
+  input CreateRacesInput{
+    raceId: ID!
+    name: String!
+    eventTimeStamp: String!
+    calendarData: calendarData
+  }
+  type Creator{
+    email: String
+    displayName: String
+    self: Boolean
+  }
+  type Organizer{
+    email: String
+    displayName: String
+    self: Boolean
+  }
+  type Time{
+    dateTime: String
+  }
+  type calendarData{
+    kind: String
+    etag: String
+    id: String
+    status: String
+    htmlLink: String
+    created: String
+    updated: String
+    summary: String
+    creator: Creator
+    organizer: Organizer
+    start: Time
+    end: Time
+    iCalUID: String
+    sequence: Int
   }
   input UpdateBoatDataInput{
     boatName: String!
@@ -31,7 +70,7 @@ const typeDefs = gql`
 `;
 const getAllBoatData = ()=> {
   let params = {
-    TableName: process.env.DYNAMODB_TABLE,    
+    TableName: "BoatData",    
     AttributesToGet: [
       'boatName',
       'pY',
@@ -43,7 +82,7 @@ const getAllBoatData = ()=> {
     // method for updates
     const changeBoatData = (args) => {
 let params = {
-  TableName: process.env.DYNAMODB_TABLE,
+  TableName: "BoatData",
   Key: { boatName: args.boatName, crew: args.crew },
   UpdateExpression: 'SET pY = :pY',
   ExpressionAttributeValues: {
@@ -57,11 +96,12 @@ return db.updateItem(params, {boatData:args})
 
 // Provide resolver functions for your schema fields
 const resolvers = {
-  Query: {
+  RootQuery: {
     allBoatData: () => getAllBoatData()
   },
-  Mutation: {
-    updateBoatData: (parent, args) => changeBoatData(args.input)
+  RootMutation: {
+    updateBoatData: (parent, args) => changeBoatData(args.input),
+    createRaces: (parent, args) => createRaces(args.input)
   }
 };
 
