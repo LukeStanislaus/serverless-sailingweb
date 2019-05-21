9d2bef5dea0531224338e89acb5d1c1a4ae1e27c
Luke Stanislaus <luke@stanislaus.co.uk>
2019-05-21 20:01:14 +0100

    cors

--- clientapp/src/apolloClient.js
+++ clientapp/src/apolloClient.js
@@ -2,5 +2,8 @@ import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
     uri: "https://hfnajybwp9.execute-api.us-east-1.amazonaws.com/dev/graphql",
-
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
 });
