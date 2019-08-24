cfb0e5ff2103ccd4efc3dbbe2f1272ed95b35570
Luke Stanislaus <luke@stanislaus.co.uk>
2019-08-24 22:18:42 +0100

    use webp

--- clientapp/src/home.js
+++ clientapp/src/home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Slider from "react-slick"
 
 const iconPath = process.env.PUBLIC_URL + 'assets/images/';
+// check_webp_feature:
+//   'feature' can be one of 'lossy', 'lossless', 'alpha' or 'animation'.
+//   'callback(feature, result)' will be passed back the detection result (in an asynchronous way!)
 
 export default () => {
+    let [webp, setWebp] = useState(true)
+    var img = new Image();
+
+    img.onerror = ()=> {
+        setWebp(false);
+    };
+    img.src = "data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
 
     var settings = {
         dots: true,
@@ -17,15 +27,15 @@ export default () => {
          autoplay: true,
          mobileFirst: true
       };
-
-    return <><Slider {...settings}><div> 
-        <img src={iconPath + "carousel-1.png"} alt={""}/>
+    return <>
+    <Slider {...settings}><div> 
+        <img src={iconPath + "carousel-1."+( webp?"webp":"png")} alt={""}/>
     </div>
     <div>
-    <img src={iconPath + "carousel-2.png"} alt={""}/>
+    <img src={iconPath + "carousel-2."+( webp?"webp":"png")} alt={""}/>
     </div>
     <div>
-    <img src={iconPath + "carousel-3.png"} alt={""}/>
+    <img src={iconPath + "carousel-3."+( webp?"webp":"png")} alt={""}/>
     </div>
 
     </Slider>
