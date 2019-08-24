372b2ce3148615e3c1b4cf5571c1636b828678f0
Luke Stanislaus <luke@stanislaus.co.uk>
2019-08-24 22:58:24 +0100

    allow installing

--- clientapp/src/serviceWorker.js
+++ clientapp/src/serviceWorker.js
@@ -21,10 +21,14 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  
+  console.log("registering");
+  if (process.env.NODE_ENV !== 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
+      
+      console.log("registering");
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
@@ -32,12 +36,16 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
+      
+      console.log("registering");
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
+      console.log("registering");
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
+        console.log("registering");
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
