43a1988ace93a0fc1547503f152fe1b78c362b39
Luke Stanislaus <luke@stanislaus.co.uk>
2022-08-21 23:22:15 +0100

    update address

--- backend/src/handler.js
+++ backend/src/handler.js
@@ -79,7 +79,7 @@ async function broadcast(event, context) {
   // broadcast the news
   const client = new AWS.ApiGatewayManagementApi({
     apiVersion: '2018-11-29',
-    endpoint: "https://705dyjpxp6.execute-api.us-east-1.amazonaws.com/dev/"
+    endpoint: "https://rhvzwuhewi.execute-api.eu-west-2.amazonaws.com/dev/graphql"
   });
   broadcastMessage = async (data) => await broadcastMessageFull(data, client)
   console.log(JSON.stringify(event))
