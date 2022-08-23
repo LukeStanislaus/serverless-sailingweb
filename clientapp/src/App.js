45b93503f2983f9ee581aa56c86565012ba37788
Luke Stanislaus <luke@stanislaus.co.uk>
2022-08-23 13:12:28 +0100

    Style updates

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -4,25 +4,42 @@ import { ApolloProvider} from '@apollo/react-hooks'
 import SignOn from './signOn'
 import ViewRace from './viewRace'
 import ManageRace from './manageRace/manageRace'
-import { } from 'react-bootstrap'
 import ManageData from './manageData'
 import Home from './home' 
 import NewPerson from './newPerson'
 import './app.css'
 import WebSocket from './WebSocket'
 import {client} from './apolloClient'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  return (<>
+  return <div style={{ paddingLeft: "3%", paddingRight: "3%" }}>
     <ApolloProvider client={client}>
-      <nav style={{ paddingLeft: "13%", paddingRight: "13%" }} className={"navbar navbar-dark bg-dark"}>
-        <Link className={"navbar-item"} to="/">Home</Link>
-        <Link className={"navbar-text"} to="/ViewRace">View Race</Link>
-        <Link className={"navbar-text"} to="/SignOn">Sign On</Link>
-        <Link className={"navbar-text"} to="/ManageRace">Manage Race</Link>
-      </nav>
-      <Router style={{ paddingLeft: "3%", paddingRight: "3%" }}>
+      
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className={"navbar-brand"} to="#">Whitefriars</Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+      <Link className={"nav-link"} to="#">Home</Link>
+      </li>
+      <li class="nav-item">
+        <Link className={"nav-link"} to="/ViewRace">View Race</Link>
+      </li>
+      <li class="nav-item">
+        <Link className={"nav-link"} to="/SignOn">Sign On</Link>
+      </li>
+      <li class="nav-item">
+        <Link className={"nav-link"} to="/ManageRace">Manage Race</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+      <Router >
         <Home path="/" />
         <ViewRace path="ViewRace" />
         <SignOn path="/SignOn" />
@@ -32,8 +49,8 @@ function App() {
       </Router>
       <WebSocket /> 
     </ApolloProvider>
-    <footer style={{ paddingLeft: "3%", paddingTop: "40px" }}><p style={{ borderTopStyle: "solid", borderTopWidth: "1px" }}>By Luke Stanislaus</p></footer>
-  </>);
+    <footer style={{ paddingTop: "40px" }}><p style={{ borderTopStyle: "solid", borderTopWidth: "1px" }}>By Luke Stanislaus</p></footer>
+  </div>;
 }
 
 
--- clientapp/src/home.js
+++ clientapp/src/home.js
@@ -24,6 +24,8 @@ export default () => {
       };
 
     return <>
+    
+        <h1 style={{paddingTop: "20px", paddingBottom: "30px"}} align={"center"}>Welcome to Whitefriars Sailing Club!</h1>
     <Slider {...settings}><div> 
         <img src={iconPath + "carousel-1."+( webp?"webp":"png")} alt={""}/>
     </div>
@@ -35,78 +37,13 @@ export default () => {
     </div>
 
     </Slider>
-    <h1 style={{paddingTop: "20px", paddingBottom: "30px"}} align={"center"}>Welcome to Whitefriars Sailing Club!</h1>
+
     <Info/>
 
     </>
 }
 
 const Info = () => {
-    return <div align={"center"} style={{paddingLeft:"30px"}} className={"row"}>
-    <div className={"col-md-3"}>
-        <h2>The Club</h2>
-        <ul>
-
-            <li><a href="http://www.whitefriarssc.org/content/about.php">About Us</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/news.php">News</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/calendar.php">Calendar</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/join_us.php">Join Us</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/contacts.php">Contacts</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/committee.php">Committee</a></li>
-            <li><a href="https://www.whitefriarssc.org/gallery/index.php">Gallery</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/webcam.php">Webcam</a></li>
-
-        </ul>
-    </div>
-    <div className={"col-md-3"}>
-        <h2>Groups</h2>
-        <ul>
-            <li><a href="http://www.whitefriarssc.org/content/juniors.php">Juniors</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/ladies.php">Ladies</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/sailability.php">Sailability</a></li>
-
-        </ul>
-    </div>
-    <div className={"col-md-3"}>
-        <h2>Training</h2>
-        <ul>
-            <li><a href="http://www.whitefriarssc.org/content/training.php">Courses in 2018</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/training_L1_L2.php">Learn To Sail</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/costs.php">How Much?</a></li>
-        </ul>
-    </div>
-    <div className={"col-md-3"}>
-        <h2>Sailing</h2>
-        <ul>
-            <li><a href="http://www.whitefriarssc.org/content/programme.php">Programme</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/results.php">Results</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/race_day_info.php">Race Info</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/day_visitors.php">Day Sailing</a></li>
-        </ul>
-    </div>
-    <div className={"col-md-3"}>
-        <h2>Resources</h2>
-        <ul>
-            <li><a href="http://www.whitefriarssc.org/content/for_sale.php">For Sale</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/catering.php">Catering</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/hireboats.php">Hire Boats</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/weather.php">Weather</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/development.php">Development Plan</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/newsletters.php">Newsletters</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/documents.php">Club Documents</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/links.php">Links</a></li>
-        </ul>
-    </div>
-    <div className={"col-md-3"}>
-        <h2>Members</h2>
-        <ul>
-            <li><a href="http://www.whitefriarssc.org/content/duty_info.php">Dutyman Intro</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/duty_rota.php">Dutyman</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/WebCol-intro.php">Update Membership</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/WebCol1stapplic.php">New Members</a></li>
-            <li><a href="https://www.whitefriarssc.org/content/constitution.php">Constitution</a></li>
-            <li><a href="http://www.whitefriarssc.org/content/rules.php">Rules</a></li>
-        </ul>
-    </div>
-</div>
+    return <div>This site allows you to join, and manage, a handicap sailing race. 
+        You probably want to sign on, the link is at the top!</div>
 }
